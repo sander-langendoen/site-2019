7d7d2d80aca23889358015c795444e211030b7b2
Sander  Langendoen <s.langendoen@simgroep.nl>
2019-01-25 14:37:44 +0100

    Added project section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import ProfilePic from '../components/Global/ProfilePic'
+import logoPCL from '../img/logo-postcodeloterij.jpg'
+import logoMZ from '../img/logo-menzis.jpg'
+import logoAZ from '../img/logo-anderzorg.jpg'
+import logoSIM from '../img/logo-sim.jpg'
 // import Typewrite from '../components/Typewrite'
 
 
@@ -19,51 +23,129 @@ export default class IndexPage extends React.Component {
 
             <section className="section section-intro">
 
-                <div className="columns"> 
-                    <div className="column">
-                        <h1 className="page-title page-title-home">Freelance (UI) front-end voor jouw project</h1>
-                        <p className="page-content-intro page-content-intro-home">
-                            Ik realiseer de HTML, CSS, Javascript (en soms PHP code) van websites of online applicaties. 
-                            Ik ben creatief, en heb oog voor design. Hoe mensen mij verder noemen?
-                        </p>
-                        <p className="page-content-intro page-content-intro-home">
-                            Je vindt hier meer &nbsp;
-                            <Link to="/over-mij">
-                              over mij
-                            </Link>.<br/>
-                            Hier lees je meer over &nbsp;
-                            <Link to="/wie-ben-jij">
-                              jezelf
-                            </Link>.<br/>
-                            <br/>
-                            Ik heb voor ontwikkelaars ook een &nbsp;
-                            <Link to="/blog">
-                              blog
-                            </Link>&nbsp; over front-end ontwikkeling.<br/>
-                            <br/>
-                            Je kunt contact met mij opnemen over jouw project. We zouden perfect voor elkaar kunnen zijn?<br/><br/>
-                            <Link className="btn-primary" to="/contact">
-                              Contact
-                            </Link>
-                        </p>
-                    </div>
-                    <div className="column">
-                        <ProfilePic />
+                <div className="container">
+
+                    <div className="columns"> 
+                        <div className="column">
+                            <h1 className="page-title page-title-home">Freelance (UI) front-end voor jouw project</h1>
+                            <p className="page-content-intro page-content-intro-home">
+                                Ik realiseer de HTML, CSS, Javascript (en soms PHP code) van websites of online applicaties. 
+                                Ik ben creatief, en heb oog voor design. Hoe mensen mij verder noemen?
+                            </p>
+                            <p className="page-content-intro page-content-intro-home">
+                                Je vindt hier meer &nbsp;
+                                <Link to="/over-mij">
+                                  over mij
+                                </Link>.<br/>
+                                Hier lees je meer over &nbsp;
+                                <Link to="/wie-ben-jij">
+                                  jezelf
+                                </Link>.<br/>
+                                <br/>
+                                Ik heb voor ontwikkelaars ook een &nbsp;
+                                <Link to="/blog">
+                                  blog
+                                </Link>&nbsp; over front-end ontwikkeling.<br/>
+                                <br/>
+                                Je kunt contact met mij opnemen over jouw project. We zouden perfect voor elkaar kunnen zijn?<br/><br/>
+                                <Link className="btn-primary" to="/contact">
+                                  Contact
+                                </Link>
+                            </p>
+                        </div>
+                        <div className="column">
+                            <ProfilePic />
+                        </div>
                     </div>
                 </div>
+            </section>
+
+            <section className="section section-projects">
+
+                <div className="container">
+                    <h2 className="section-title" style={{ textAlign: 'center' }}>Recente opdrachten</h2>
+
+                    <div className="article-list">
+
+                        <article
+                              className="content"
+                              style={{ backgroundColor: '#fff' }}
+                            >
+                            <h3 style={{float: "left"}}>SIMGroep</h3>
+                            <img src={logoSIM} alt="logo SIMgroep" style={{float: "left", paddingLeft: "10px", maxWidth: "65px"}} />
+
+                            <p style={{clear: "both"}}>
+                                Bij de SIMGroep mocht ik iedere dag voor miljoenen Nederlanders hun 'online te regelen zaken', 
+                                met de overheid, makkelijker maken. Dit heb ik gedaan door te werken aan de front-end van 
+                                digitale producten voor diverse gemeenten en andere overheidsinstellingen in Nederland, 
+                                Aruba en Curacao.
+                            </p>
+                            
+                            <p> Denk aan websites, social intranets of andere digitale oplossingen zoals 
+                                het inbouwen van extra toegankelijksheidseisen voor de overheid zoals Readspeaker. Ook heb 
+                                ik voor het development team een workshop Flexbox mogen organiseren. Ik heb ook de LESS-architectuur 
+                                voor hun sociale intranetten opnieuw mogen opzetten. Hiernaast heb ik ook de SASS-structuren voor 
+                                onze afdeling opnieuw mogen opzetten, en tevens Gulp hier geïntroduceerd. 
+                            </p>
+                            <p><strong>Opdrachtgever</strong><br/>SIMGroep</p>
+                            
+                            <p><strong>Keywords:</strong><br/> PHP, HTML5, Javascript, SASS, CSS3, Flexbox, Javascript, jQuery, webrichtlijnen</p>
+                        </article>
+
+                        <article
+                              className="content"
+                              style={{ backgroundColor: '#fff' }}
+                            >
+                            <h3 style={{float: "left"}}>Postcode Loterij <br/>BankGiro Loterij <br/>Vriendenloterij</h3>
+                            <img src={logoPCL} alt="logo Postcode Loterij" style={{float: "left", paddingLeft: "10px", maxWidth: "65px"}} />
+
+                            <p style={{clear: "both"}}>
+                                Voor award winning digital agency The Valley in Amsterdam werkte ik 
+                                voor klanten als de Nationale Postcodeloterij, BankGiro Loterij en 
+                                de Vriendenloterij. De werkzaamheden betroffen het opzetten van Landingspages 
+                                en campagne websites voor verschillende online campagnes.
+                            </p>
+                            
+                            <p><strong>Opdrachtgever</strong><br/>The Valley</p>
+                            
+                            <p><strong>Keywords:</strong><br/> Javascript (o.a. Moustache JS), HTML5, CSS3 (Sass), Git, Gulp, Jira, Fabricator</p>
+                        </article>
+
+                        <article
+                              className="content"
+                              style={{ backgroundColor: '#fff' }}
+                            >
+                            <h3 style={{float: "left"}}>Menzis <br/>Anderzorg</h3>
+                            <img src={logoMZ} alt="logo Menzis" style={{float: "left", paddingLeft: "10px", maxWidth: "65px"}} />
+                            <img src={logoAZ} alt="logo Anderzorg" style={{float: "left", paddingLeft: "10px", maxWidth: "65px"}} />
+
+                            <p style={{clear: "both"}}>
+                                Voor de verzekeraars Menzis en Anderzorg werden door Yourzine e-mailcampagnes opgezet. 
+                                Voor deze campagnes moesten landingspagina's met verschillende tabbladen worden opgezet. 
+                                Hiervoor was ik samen met twee andere ontwikkelaars verantwoordelijk. De meeste uitdaging 
+                                zat in de conditionele data die we middels PHP per doelgroep toonden op de landingspagina.
+                            </p>
+                            
+                            <p><strong>Opdrachtgever</strong><br/>Yourzine</p>
+                            
+                            <p><strong>Keywords:</strong><br/> PHP, Javascript (jQuery), HTML5, CSS3 (Sass), Git, Gulp, Trello</p>
+                        </article>
+                    </div>
+                </div>
+
             </section>
 
             <section className="section section-articles">
 
                 <div className="container">
-                    <h2 className="section-title">Mijn laatste blog-posts</h2>
+                    <h2 className="section-title" style={{ textAlign: 'center' }}>Mijn laatste blog-posts</h2>
 
                     <div className="article-list">
                         {posts
                           .map(({ node: post }) => (
                             <article
                                   className="content"
-                                  style={{ padding: '2rem 4rem 2rem 0rem' }}
+                                  style={{ backgroundColor: '#fff' }}
                                   key={post.id}
                                 >
                                 <h3>
