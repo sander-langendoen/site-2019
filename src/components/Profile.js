c47f8702208ae7afacc0f753442eb26c3640f1f0
Sander  Langendoen <s.langendoen@simgroep.nl>
2019-04-22 10:40:36 +0200

    styling profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link} from 'gatsby'
 
 import ProfilePic from './Global/ProfilePic'
 
@@ -8,12 +9,23 @@ const Profile = class extends React.Component {
 
 		return (
 
-			<section className="profile">
-				<ProfilePic />
-				<h3>Freelance (UI) front-end voor jouw project</h3>
-				<p>Ik realiseer de HTML, CSS, Javascript (en soms PHP code) van websites of online applicaties. 
-				Ik ben creatief, en heb oog voor design. Hoe mensen mij verder noemen?</p>
-				<p>Je vindt hier meer &nbsp;<a href="/over-mij">over mij</a>.</p>
+			<section className="section-profile">
+				<div className="container">
+					<h2>Over Sander Langendoen</h2>
+					<ProfilePic />
+					<h3>Freelance (UI) front-end voor jouw project</h3>
+					<p>Ik realiseer de HTML, CSS, Javascript (en soms PHP code) van websites of online applicaties. 
+					Ik ben creatief, en heb oog voor design. Hoe mensen mij verder noemen?</p>
+					
+					<p>Je vindt hier meer &nbsp;
+                        <Link to="/over-mij">
+                          over mij
+                        </Link>.
+                    </p>
+                    <p>Of neem &nbsp;
+                        <Link to="/contact">contact</Link> met me op.
+                    </p>
+				</div>
 			</section>
 		)
 	}
